bef6ac31b00f0e66cd1ac07a9c7b37e533bf9c16
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-07-10 22:28:47 +0800

    AR Dice app

--- AR Dice/ViewController.swift
+++ AR Dice/ViewController.swift
@@ -20,5 +20,26 @@ class ViewController: UIViewController {
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
+        
+        boxAnchor.actions.throwDice.onAction = { entity in
+            let dice = entity as! HasPhysics
+            
+            dice.physicsBody?.mode = .kinematic
+            
+            dice.physicsMotion?.angularVelocity = [
+                Float.random(in: 0 ..< 4 * .pi),
+                Float.random(in: 0 ..< 4 * .pi),
+                Float.random(in: 0 ..< 4 * .pi)
+            ]
+            
+            dice.physicsMotion?.linearVelocity = [0, 1, 0]
+            
+            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
+                dice.physicsBody? .mode = . dynamic
+                dice.physicsMotion?.angularVelocity = [0, 0, 0]
+                
+                dice.physicsMotion?.linearVelocity = [0, 0, 0]
+            }
+        }
     }
 }
